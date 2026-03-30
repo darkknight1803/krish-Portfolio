c5d63350b1665392685afa6f409069765d232552
darkknight1803 <darkknight1803>
2026-03-30 16:09:40 +0530

    chore: setup github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/krish-Portfolio/",
   server: {
     host: "::",
     port: 8080,
